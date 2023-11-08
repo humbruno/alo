1b148b07faba411ef23530544026c280c6f2fb6c
Bruno Santos <bruno.santos@sitewerk.ch>
2023-11-08 09:55:50 +0000

    test with import schema

--- api/calculate.ts
+++ api/calculate.ts
@@ -1,19 +1,19 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-
 import { z } from 'zod';
+import { calculateFormSchema } from '../src/validations/index';
 
-const calculateFormSchema = z.object({
-  firstName: z.string().optional(),
-  lastName: z.string().optional(),
-  company: z.string().optional(),
-  email: z.string().email('calculateForm.invalidEmail'),
-  phone: z.string({ invalid_type_error: 'calculateForm.requiredField' }),
-  postCode: z.string().min(1, 'calculateForm.requiredField'),
-  city: z.string().optional(),
-  disclaimer: z
-    .boolean({ required_error: 'calculateForm.requiredField' })
-    .refine((data) => data === true),
-});
+// const calculateFormSchema = z.object({
+//   firstName: z.string().optional(),
+//   lastName: z.string().optional(),
+//   company: z.string().optional(),
+//   email: z.string().email('calculateForm.invalidEmail'),
+//   phone: z.string({ invalid_type_error: 'calculateForm.requiredField' }),
+//   postCode: z.string().min(1, 'calculateForm.requiredField'),
+//   city: z.string().optional(),
+//   disclaimer: z
+//     .boolean({ required_error: 'calculateForm.requiredField' })
+//     .refine((data) => data === true),
+// });
 
 export default async function (
   request: VercelRequest,
